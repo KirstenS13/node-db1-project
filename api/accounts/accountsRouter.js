12614ebfa5675e1825b493ea5c7260b654f5f327
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-19 20:21:21 -0400

    set up accountsRouter and made welcome endpoint

---
+++ api/accounts/accountsRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('../../data/dbConfig');
+
+const router = express.Router();
+
+// endpoints
+
+// get all accounts
+
+// get accounts by id
+
+// create account
+
+// update account
+
+// delete account
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const accountsRouter = require('./accounts/accountsRouter');
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/accounts", accountsRouter);
+
+// welcome message
+server.get("/", (req, res) => {
+    res.json("Welcome")
+})
 
 module.exports = server;
